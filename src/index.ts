eb39a9c2f253c2603afe41f5eb5ede2942c2a04f
msandypr <msandypr.dev@gmail.com>
2025-03-10 15:07:26 +0700

    feat(main): revamping the landing page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+// Author: Muhammad Sandy Putra Riyadi ~ @msandypr
 import express, { Express, Request, Response } from "express";
+import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,9 +8,19 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server by @msandypr");
+app.use(express.static('public'));
+app.get("/applicationName", (req: Request, res: Response) => {
+    try {
+        const appName = process.env.APPLICATION_NAME;
+        res.json({ applicationName: appName });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+app.get("/", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
